f8cc3b7d4d8592a53546ec6935d07c61e0531ed5
Jady Wright <iamjwright159@gmail.com>
2024-03-14 13:59:13 -0400

    Add unit testing framework

---
+++ Echidna2.Tests/NotificationTests.cs
@@ -0,0 +1,10 @@
+namespace Echidna2.Tests;
+
+public class NotificationTests
+{
+	[Fact]
+	public void ClickingInButton_WhenButtonIsInvisible_DoesntActivateButton()
+	{
+		throw new NotImplementedException();
+	}
+}
