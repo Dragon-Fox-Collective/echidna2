85f0a629396c8d97f0e4cfc2f608740e468ddc5c
Jady Wright <iamjwright159@gmail.com>
2024-04-29 11:39:44 -0400

    Move test prefabs into prefabs folder

---
+++ Echidna2.Tests/Prefabs/Test/SubcomponentPrefab.cs
@@ -0,0 +1,9 @@
+﻿using Echidna2.Core;
+using Echidna2.Serialization;
+
+namespace Echidna2.Prefabs.Test;
+
+public partial class SubcomponentPrefab
+{
+	[SerializedReference, ExposeMembersInClass] public SubcomponentSubcomponent Subcomponent { get; set; } = null!;
+}
---
+++ Echidna2.Tests/Prefabs/Test/SubcomponentSubcomponent.cs
@@ -0,0 +1,9 @@
+﻿using Echidna2.Core;
+using Echidna2.Serialization;
+
+namespace Echidna2.Prefabs.Test;
+
+public partial class SubcomponentSubcomponent
+{
+	[SerializedReference, ExposeMembersInClass] public object? Reference { get; set; } = null!;
+}
